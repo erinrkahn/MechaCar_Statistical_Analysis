1ce77e0d31bbaea7a618f776c28c3fe4a2f9e551
erinrkahn <77405273+erinrkahn@users.noreply.github.com>
2021-05-15 12:14:46 -0700

    add total_summary and lot_summary table to MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,20 @@
+# Deliverable 1
+
 library(dplyr)
 MechaCar_mpg <- read.csv('MechaCar_mpg.csv')
 mpg_LR = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
 summary(mpg_LR)
+
+# Deliverable 2
+
+# Total Summary
+
+suspension_coil <- read.csv('Suspension_Coil.csv')
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Lot Summary
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Deliverable 3
+
