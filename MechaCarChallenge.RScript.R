c8863f9993f97d0eae793523d00730fa068d324f
erinrkahn <77405273+erinrkahn@users.noreply.github.com>
2021-05-16 08:01:19 -0700

    Add deliverable 3 code to MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -18,3 +18,10 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
 
 # Deliverable 3
 
+t.test(suspension_coil$PSI, mu=1500)
+
+t.test(subset(suspension_coil,Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+
+t.test(subset(suspension_coil,Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+
+t.test(subset(suspension_coil,Manufacturing_Lot == "Lot3")$PSI, mu=1500)
